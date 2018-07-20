f5e92c8fdf03392a60ee07edc020cde778471b0c
kegranados1@gmail.com <kegranados1@gmail.com>
2018-07-20 02:59:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === logWhisper(string)) {
   return "I can't hear you!" } 
-  else if (string ==== logShout(string)) {
+  else if (string === logShout(string)) {
     return "YES, INDEED!" }
     else if (string === "I love you, Grandma.") {
       return "I love you, too"
